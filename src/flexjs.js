dcd9726c484792a79492e2d15af48b89269faec6
zombieJ <smith3816@gmail.com>
2014-01-16 00:37:08 +0800

    check editable

--- src/flexjs.js
+++ src/flexjs.js
@@ -1,8 +1,8 @@
 /***
 	flexjs(srcTmpl).getInstance(object)
 	render svg component by template "srcTmpl" bind with data "object".
-	object:			null		bind no data.
-					
+	object:			null			bind no data.
+					object			bind object to svg
 ***/
 
 (function() {
@@ -20,19 +20,49 @@
 
 			var _instance = _elment.cloneNode(true);
 
-			// bind data of object
-			for(var _key in data) {
-				var _val = data[_key];
-				if(typeof _val != 'function') {
-					var _ele = _instance.querySelector("[data-bind-target='" + _key + "']");
-					if(_ele != null) {
-						_ele.textContent = _val;
+			// update data to svg
+			_instance.update = function() {
+				for(var _key in data) {
+					var _val = data[_key];
+					if(typeof _val != 'function') {
+						var _eles = _instance.querySelectorAll("[data-bind-target='" + _key + "']");
+						for(var i in _eles) {
+							_eles[i].textContent = _val;
+						}
 					}
 				}
+			}
+
+			// bind data of object
+			_instance.update();
+
+			// get & set binded object
+			_instance.object = function(obj) {
+				if(obj != null) {
+					data = obj;
+					_instance.update();
+				}
+				return data;
+			}
+
+			// get & set attr
+			_instance.attr = function(key, val) {
+				if(val != null) {
+					data[key] = val;
+					_instance.update();
+				}
+				return data[key];
 			}
 
 			return _instance;
 		}
 		return this;
 	}
+
+	document.addEventListener("click",function(event) {
+		var target = event.target;
+		if(target.attributes["data-bind-editable"] != null) {
+			console.log("editable!!!");
+		}
+	}, false);
 })();
